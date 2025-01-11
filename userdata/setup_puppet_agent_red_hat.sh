37c2d218b581c5dfdc7f8d0c094153b1f12dc3ea
Ismael qouiqa <qouiqaismael890@address.com>
2025-01-11 10:28:48 +0100

    fix me

--- userdata/setup_puppet_agent_red_hat.sh
+++ userdata/setup_puppet_agent_red_hat.sh
@@ -1,9 +1,9 @@
 sudo rpm -Uvh https://yum.puppet.com/puppet8-release-el-8.noarch.rpm
 
-sudo yum install puppet-agent
+sudo yum -y install puppet-agent
 
- sudo /opt/puppetlabs/bin/puppet --version
+sudo /opt/puppetlabs/bin/puppet --version
 
-echo $(hostname -i) puppet >> /etc/hosts
+sudo echo $(hostname -i) puppet >> /etc/hosts
 
 sudo /opt/puppetlabs/bin/puppet ssl bootstrap
